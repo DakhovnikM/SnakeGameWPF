377e611c13b224769f3dc5ad854ec4b27aa87938
dakhovnikMaksim <dahovnik@gmail.com>
2020-11-23 01:01:57 +0200

    Добавлено диалоговое окно

---
+++ SnakeGameWPF/Views/DialogWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnakeGameWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for DialogWindow.xaml
+    /// </summary>
+    public partial class DialogWindow : Window
+    {
+        readonly MainWindow _mainWindow;
+        public string DialogContent { get; set; }
+        public DialogWindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+
+            DialogContent = "Старт/Стоп - клавиша Пробел.\nДвижение стрелками.";
+            Label.Content = DialogContent;
+            Button.Content = "OK";
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            _mainWindow.ClearBlur();
+            _mainWindow.Btn_Exit.IsEnabled = true;
+        }
+
+    }
+}
