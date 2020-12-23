037956d73bf56bd4b7e8764da22475672325be17
Maxim_D <dahovnik@gmail.com>
2020-12-23 20:32:19 +0200

    Добавлены классы Command и RelayCommand.

---
+++ SnakeGameWPF/Commands/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGameWPF.Commands
+{
+    class RelayCommand : Command
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
